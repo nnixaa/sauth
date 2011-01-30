ea6922edacecd6f8972a66739e06ca5b470d9f75
nnixaa <4dmitr@gmail.com>
2011-01-30 17:15:18 +0200

    Setting demo example

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,11 +10,33 @@ class IndexController extends Zend_Controller_Action {
     }
     
     public function indexAction() {
+        $googleAuth = new SAuth_Provider_Google($this->config['google']);
+        $twitterAuth = new SAuth_Provider_Twitter($this->config['twitter']);
+        if ($googleAuth->isAuthorized() || $twitterAuth->isAuthorized()) {
+            $this->view->isAuth = true;
+            if ($googleAuth->isAuthorized()) {
+                $this->view->id = $googleAuth->getAuthId();
+            }
+            if ($twitterAuth->isAuthorized()) {
+                $this->view->id = $twitterAuth->getAuthId();
+            }
+        } else {
+            $this->view->isAuth = false;
+        }
     }
     
     public function authAction() {
-        $googleAuth = new SAuth_Provider_Google($this->config);
-        $this->view->auth = $googleAuth->auth();
+        $authBy = $this->getResponse()->getParam('by') ? $this->getResponse()->getParam('by') : 'google';
+        switch ($authBy) {
+            case 'google':
+                $googleAuth = new SAuth_Provider_Google($this->config['google']);
+                $this->view->auth = $googleAuth->auth();
+                break;
+            case 'twitter':
+                $twitterAuth = new SAuth_Provider_Twitter($this->config['twitter']);
+                $this->view->auth = $twitterAuth->auth();
+                break;
+        }
     }
     
     public function logoutAction() {
