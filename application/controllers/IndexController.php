737461d11c55b4db47aa01ea5b82bf61b67b0435
nnixaa <4dmitr@gmail.com>
2011-01-30 16:13:54 +0200

    Working example for google openid

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,6 +5,7 @@ class IndexController extends Zend_Controller_Action {
     public $config = array();
     
     public function init() {
+        
         $this->config = array(
             'id' => 'https://www.google.com/accounts/o8/id',
             'callbackUrl' => '/index/auth',
@@ -12,17 +13,25 @@ class IndexController extends Zend_Controller_Action {
     }
     
     public function indexAction() {
+        
+        $googleAuth = new SAuth_Provider_Google($this->config);
+        if (!$googleAuth->isAuthorized()) {
+            $this->view->auth = false;
+        } else {
+            $this->view->auth = true;
+            $this->view->googleId = $googleAuth->getAuthId();
+        }
     }
     
     public function authAction() {
-        $auth = new SAuth_Provider_Google($this->config);
-        if ($auth->auth()) {
-            Zend_Debug::dump($auth->getAuthId());
-        }
+        $googleAuth = new SAuth_Provider_Google($this->config);
+        $this->view->auth = $googleAuth->auth();
     }
     
     public function logoutAction() {
         $this->_helper->viewRenderer->setNoRender();
+        $googleAuth = new SAuth_Provider_Google($this->config);
+        $googleAuth->clearAuth();
         $this->getResponse()->setRedirect('/');
     }
 
