9cf82cb45a519535b140c3687ba8ce30e3050ff6
nixa <4dmitr@gmail.com>
2011-02-12 14:47:50 +0200

    Provider singleton

--- demos/application/controllers/IndexController.php
+++ demos/application/controllers/IndexController.php
@@ -4,18 +4,58 @@ class IndexController extends Zend_Controller_Action {
 
     public $config = array();
     
+    
     public function init() {
+        
         $this->config = Zend_Registry::get('sauthConf');
+        
     }
     
     public function indexAction() {
+        
+        $this->view->vkAppId = $this->config['vkontakte']['consumerId'];
+        $this->view->vkAuthUrl = $this->config['vkontakte']['userAuthorizationUrl'];
+        
+        $sauthProvider = new SAuth_Provider();
+        if ($sauthProvider->isAuthorized()) {
+                
+            $this->view->auth = true;
+            $this->view->parameters = $sauthProvider->getCurrentProvider()->getUserParameters();
+        
+        } else {
+            
+            $this->view->auth = false;
+        }
     }
     
     public function authAction() {
-        $authBy = $this->getRequest()->getParam('by') ? $this->getRequest()->getParam('by') : 'google';
+            
+        $provider = $this->getRequest()->getParam('by') ? $this->getRequest()->getParam('by') : 'google';
+        $providerClass = 'SAuth_Provider_' . ucfirst($provider);
+        
+        $sauthProvider = new SAuth_Provider();
+        $sauthProvider->setUpProvider($providerClass, $this->config[$provider]);
+        
+        if ($sauthProvider->authenticate()) {
+            
+            $this->view->auth = true;
+            
+        } else {
+            
+            $this->view->auth = false;
+            
+        }
+        
     }
     
     public function logoutAction() {
+        
+        $sauthProvider = new SAuth_Provider();
+        
+        if ($sauthProvider->isAuthorized()) {
+            $sauthProvider->getCurrentProvider()->clearAuth();
+        }
+        
         $this->_helper->viewRenderer->setNoRender();
         $this->getResponse()->setRedirect('/');
     }
--- library/SAuth/Provider.php
+++ library/SAuth/Provider.php
@@ -12,4 +12,128 @@ class SAuth_Provider {
     const MAILRU = 'SAuth_Provider_Mailru';
     const VKONTAKTE = 'SAuth_Provider_Vkontakte';
     
+    /**
+     * @var Zend_Session_Namespace
+     */
+    protected $_currentProviderStorage = null;
+    
+    /**
+     * @var string Current provider class name
+     */
+    protected static $_currentProviderName = '';
+    
+    /**
+     * @var SAuth_Provider
+     */
+    protected static $_currentProviderObject = null;
+    
+    /**
+     * @var bool
+     */
+    protected $_isSetuped = false;
+    
+    /**
+     * Object constructor method
+     */
+    public function __construct() {
+        
+        $this->_currentProviderStorage = new Zend_Session_Namespace('SAUTH_PROVIDER');
+        
+        if (!empty($this->_currentProviderStorage->className)) {
+                
+            $this->setUpProvider($this->_currentProviderStorage->className, $this->_currentProviderStorage->configuration);
+        }
+    }
+    
+    /**
+     * Check auth
+     * @return bool 
+     */
+    public function isAuthorized() {
+        
+        if ($this->isSetuped() && $this->getCurrentProvider()->isAuthorized()) {
+            return true;
+        }
+        
+        return false;
+    }
+    
+    /**
+     * Setup provider
+     * @param string $class
+     * @param array $configuration
+     * @return Sauth_Providre|false
+     */
+    public function setUpProvider($class, array $configuration = array()) {
+        
+        $this->setCurrentProviderName($class);
+        
+        Zend_Loader::loadClass($class);
+        $provider = new $class();
+        
+        if ($provider instanceof SAuth_Provider_Interface) {
+            
+            $provider->setConfig($configuration);
+            
+            $this->_currentProviderStorage->configuration = $configuration;
+            $this->_currentProviderStorage->className = $class;
+            
+            $this->_isSetuped = true;
+            
+            return $this->_currentProviderObject = $provider;
+        }
+        return false;
+    }
+    
+    /**
+     * Check is provider class has been setuped
+     * @return bool
+     */
+    public function isSetuped() {
+        
+        return $this->_isSetuped;
+        
+    }
+    
+    /**
+     * Authenticate user
+     * @return bool
+     */
+    public function authenticate() {
+        
+        if (!$this->isSetuped()) {
+            throw new SAuth_Exception('You should run setUpProvider() before authenticate');
+        }
+        return $this->getCurrentProvider()->authenticate();
+    }
+    
+    /**
+     * Returns current provider object
+     * @var Sauth_provider
+     */
+    public function getCurrentProvider() {
+        
+        $provider = $this->_currentProviderObject;
+        return $provider;
+    }
+    
+    /**
+     * Return current provider class name
+     * @return string
+     */
+    public function getCurrentProviderName() {
+        
+        return $this->_currentProviderName;
+    }
+
+    /**
+     * Sets current provider class name
+     * @return string
+     */
+    public function setCurrentProviderName($class) {
+        
+        $class = (string) $class;
+        return $this->_currentProviderName = $class;
+    }
+    
 }
