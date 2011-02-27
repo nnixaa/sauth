c2479dd88a053df03fa77db0a09c553bb6901af1
nixa <4dmitr@gmail.com>
2011-02-27 13:18:37 +0200

    Aded siteUrl parameter

--- demos/application/Bootstrap.php
+++ demos/application/Bootstrap.php
@@ -13,9 +13,15 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
      */
     protected function _initSauth() {
         
+        /**
+         * @see application.ini
+         */
+        $siteDir = $this->getOption('siteDir');
+        $siteUrl = $this->getOption('siteUrl');
+        
         $sauthConf['google'] = array(
             'id' => 'https://www.google.com/accounts/o8/id',
-            'callbackUrl' => '/index/auth/by/google',
+            'callbackUrl' => $siteUrl . $siteDir . '/index/auth/by/google',
             'exchangeExtension' => array(
                 'openid.ns.ax' => 'http://openid.net/srv/ax/1.0',
                 'openid.ax.mode' => 'fetch_request',
@@ -27,14 +33,13 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $sauthConf['twitter'] = array(
             'consumerKey' => '',
             'consumerSecret' => '',
-            'callbackUrl' => 'http://dnixa.tmweb.ru/index/auth/by/twitter',
+            'callbackUrl' => $siteUrl . $siteDir . '/index/auth/by/twitter',
         );
           
         $sauthConf['facebook'] = array(
             'consumerId' => '',
-            'consumerKey' => '',
             'consumerSecret' => '',
-            'callbackUrl' => 'http://dnixa.tmweb.ru/index/auth/by/facebook',
+            'callbackUrl' => $siteUrl . $siteDir . '/index/auth/by/facebook',
             'scope' => array(
                  'user_about_me', 'email',
             ),
@@ -43,21 +48,28 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $sauthConf['vkontakte'] = array(
             'consumerId' => '',
             'consumerSecret' => '',
-            'userAuthorizationUrl' => 'http://dnixa.tmweb.ru/index/auth/by/vkontakte',
-            'callbackUrl' => 'http://dnixa.tmweb.ru',
+            'userAuthorizationUrl' => $siteUrl . $siteDir . '/index/auth/by/vkontakte',
+            'callbackUrl' => $siteUrl . $siteDir,
         );
         
         $sauthConf['mailru'] = array(
             'consumerId' => '',
             'privateKey' => '',
             'consumerSecret' => '',
-            'callbackUrl' => 'http://dnixa.tmweb.ru/index/auth/by/mailru',
+            'callbackUrl' => $siteUrl . $siteDir . 'index/auth/by/mailru',
         );
         
         $sauthConf['foursquare'] = array(
             'consumerSecret' => '',
             'consumerId' => '',
-            'callbackUrl' => 'http://dnixa.tmweb.ru/index/auth/by/foursquare',
+            'callbackUrl' => $siteUrl . $siteDir . '/index/auth/by/foursquare',
+        );
+        
+        $sauthConf['flickr'] = array(
+            'consumerKey' => '',
+            'consumerSecret' => '',
+            'userAuthorizationUrl' => 'http://flickr.com/services/auth/',
+            'permission' => SAuth_Adapter_Flickr::PERMS_READ,
         );
         Zend_Registry::set('sauthConf', $sauthConf);
         
