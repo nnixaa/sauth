bca474997945f8cab62cc553ca81854d6f15eec4
nixa <4dmitr@gmail.com>
2011-03-13 14:46:40 +0200

    Draft version of github auth - adapter

--- library/SAuth/Adapter/Github.php
+++ library/SAuth/Adapter/Github.php
@@ -18,6 +18,14 @@ require_once 'Zend/Auth/Adapter/Interface.php';
 class SAuth_Adapter_Github extends SAuth_Adapter_Abstract implements Zend_Auth_Adapter_Interface {
 
     /**
+     * Scopes let you specify exactly what type of access you need
+     */
+    const SCOPE_USER        = 'user';
+    const SCOPE_PUBLIC_REPO = 'public_repo';
+    const SCOPE_REPO        = 'repo';
+    const SCOPE_GIST        = 'gist';
+    
+    /**
      * @var array Configuration array
      */
     protected $_config = array(
@@ -27,6 +35,9 @@ class SAuth_Adapter_Github extends SAuth_Adapter_Abstract implements Zend_Auth_A
         'userAuthorizationUrl'  => 'https://github.com/login/oauth/authorize',
         'accessTokenUrl'        => 'https://github.com/login/oauth/access_token',
         'requestDatarUrl'       => 'https://github.com/api/v2/json/user/show',
+        'scope'                 => array(
+            self::SCOPE_USER
+         ),
     );
     
     /**
@@ -67,25 +78,24 @@ class SAuth_Adapter_Github extends SAuth_Adapter_Abstract implements Zend_Auth_A
             $response = $this->httpRequest('POST', $accessTokenUrl, $accessConfig);
             
             if ($response->isError()) {
-                //gowalla return 400 http code on error
-                switch  ($response->getStatus()) {
-                    case '400':
-                        $parsedErrors = $this->parseResponseJson($response->getBody());
-                        $error = $parsedErrors['detail'];
-                        break;
-                    default:
-                        $error = 'Github Oauth service unavailable';
-                        break;
-                }
-
+                    
+                $error = 'Github Oauth service unavailable';
                 return new Zend_Auth_Result(Zend_Auth_Result::FAILURE, false, array($error));
                 
             } elseif ($response->isSuccessful()) {
+
+                $parsedResponse = $this->parseResponseUrl($response->getBody());
                 
-                $parsedResponse = $this->parseResponseJson($response->getBody());
+                if (isset($parsedResponse['error'])) {
+                    
+                    return new Zend_Auth_Result(Zend_Auth_Result::FAILURE, false, array($parsedResponse['error']));
+                }
                 
-                $identity = $this->_prepareIdentity($parsedResponse);
+                //try to get user data
+                $userParameters = (array) $this->requestUserParams($parsedResponse['access_token']);
                 
+                $identity = $this->_prepareIdentity(array_merge($parsedResponse, $userParameters));
+
                 return new Zend_Auth_Result(Zend_Auth_Result::SUCCESS, $identity);
                 
             }
@@ -95,7 +105,11 @@ class SAuth_Adapter_Github extends SAuth_Adapter_Abstract implements Zend_Auth_A
                 'client_id'     => $clientId, 
                 'redirect_uri'  => $redirectUrl,
             );
-
+            
+            if (isset($config['scope']) && !empty($config['scope'])) {
+                $authorizationConfig['scope'] = implode($config['scope'], ',');
+            }
+            
             $url = $authorizationUrl . '?';
             $url .= http_build_query($authorizationConfig, null, '&');
             header('Location: ' . $url);
@@ -115,6 +129,22 @@ class SAuth_Adapter_Github extends SAuth_Adapter_Abstract implements Zend_Auth_A
      */
     public function requestUserParams($accessToken) {
         
+        $apiUrl = $this->getConfig('requestDatarUrl');
+
+        if ($accessToken && !empty($apiUrl)) {
+            
+            $response = $this->httpRequest('GET', $apiUrl, array('access_token' => $accessToken));
+            
+            if ($response->isError()) {
+                // TODO: maybe will better return an error?
+                // $parsedErrors = (array) $this->parseResponseJson($response->getBody());
+                return false;
+            } elseif ($response->isSuccessful()) {
+                $parsedResponse = (array) $this->parseResponseJson($response->getBody());
+                return $parsedResponse;
+            }
+        }
+        return false; 
     }
     
 }
